6f752079e2ba2a801f4e517af6dd3b0e93511270
keyarahman <keya91bd@gmail.com>
2025-02-20 16:54:47 +0600

    date picker modal ui added

--- app/(attendance)/punchLogs.jsx
+++ app/(attendance)/punchLogs.jsx
@@ -1,16 +1,21 @@
-import { View, Text,StatusBar } from 'react-native'
-import React from 'react'
+import { View, Text, StatusBar, Button } from 'react-native'
+import React, { useRef } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AttendanceHeader from '../../components/attendance/AttendanceHeader'
 
 const punchLogs = () => {
-  return (
-    <SafeAreaView>
-        <StatusBar  backgroundColor={"#1680E1"} barStyle={"light-content"} />
-        <AttendanceHeader pageName="Punch Logs" />
-      <Text>punchLogs</Text>
-    </SafeAreaView>
-  )
+    const bottomSheetRef = useRef(null);
+
+    return (
+        <SafeAreaView>
+            <StatusBar backgroundColor={"#1680E1"} barStyle={"light-content"} />
+            <AttendanceHeader pageName="Punch Logs" />
+            <Text>punchLogs</Text>
+
+
+        
+        </SafeAreaView>
+    )
 }
 
 export default punchLogs
--- components/attendance/AttendanceHeader.jsx
+++ components/attendance/AttendanceHeader.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import LeftIcon from "../../assets/AttendaceIocns/LeftIcon.svg";
 import BellIcon from "../../assets/AttendaceIocns/BellIcon.svg";
 import { useRouter } from "expo-router";
+import DatePickerModal from "./components/attendance/DatePickerModal";
+
 const AttendanceHeader = ({ pageName, onNotificationPress }) => {
   const router = useRouter();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  // Function to format the date as "19-12 May"
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return `${date.getDate()}-${date.getMonth() + 1} ${date.toLocaleString('en-US', { month: 'short' })}`;
+  };
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
@@ -29,8 +39,26 @@ const AttendanceHeader = ({ pageName, onNotificationPress }) => {
           <BellIcon />
         </TouchableOpacity>
       </View>
-{/* {add code here for monnth picker} */}
-      
+      <TouchableOpacity style={styles.monthContainer} onPress={() => setModalVisible(true)}>
+        {startDate && endDate ? (
+          <Text style={{ color: "black", fontSize: 10, fontWeight: "500" }}>
+                        Selected: {startDate ? formatDate(startDate) : "Start Date"} - {endDate ? formatDate(endDate) : "End Date"}
+                        </Text>
+        ) : (
+          <Text style={{ color: "black", fontSize: 10, fontWeight: "500" }}>This Month</Text>
+        )}
+      </TouchableOpacity>
+
+      <DatePickerModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onConfirm={(startDate,endDate) => {
+          console.log("Selected Dates:", startDate, endDate);
+          setStartDate(startDate);
+          setEndDate(endDate)
+          setModalVisible(false);
+        }}
+      />
     </View>
   );
 };
@@ -65,6 +93,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "flex-end",
   },
+  monthContainer: { backgroundColor: "#C1CAC7", paddingHorizontal: 20, paddingVertical: 8, borderRadius: 40, alignSelf: "flex-start", marginTop: 30 }
 });
 
 export default AttendanceHeader;
